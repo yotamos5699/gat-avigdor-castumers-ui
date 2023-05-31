2546563d7acc4a49c358e48caf66ea8c7d56aa0b
yotamos5699 <m.lhafakot@gmail.com>
2023-05-31 10:52:00 +0300

    sd

--- src/pages/mainDashboard/modules/index.tsx
+++ src/pages/mainDashboard/modules/index.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { Nav } from "..";
+import { useQuery } from "@tanstack/react-query";
 
 function Modules() {
   const [currentData, setCurrentData] = useState<string>("modules");
+
   return (
     <div>
       <Nav setCurrentData={setCurrentData} currentData={currentData} />
-      index
     </div>
   );
 }
 
-export default Modules;
+export default Modules
