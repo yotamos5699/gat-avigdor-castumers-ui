5d9452e9f763dbc61d3f6ed1e784f402042dea36
yotamos5699 <m.lhafakot@gmail.com>
2023-05-31 12:59:59 +0300

    s

--- src/components/mainDashboard/utils/helper.tsx
+++ src/components/mainDashboard/utils/helper.tsx
@@ -1,5 +1,12 @@
+import { useRouter } from "next/router";
 import { Module_ } from "~/pages/mainDashboard/modules";
 
 export const go2App = ({ m }: { m: Module_ }) => {
   console.log({ m });
+  const { push } = useRouter();
+  switch (m.שם) {
+    case "biziRows": {
+      push("/biziRows");
+    }
+  }
 };
